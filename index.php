b721d8c24d0b98925dcd4333b0975f00878bc8c2
Luis Funes <lfnz999@gmail.com>
2017-06-05 23:37:37 -0500

    Inicio de Projecto
    Se empieza a crear la base del projecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    include('includes/header.php');
+    include('includes/nav.php');
+?>
+    <div class="container">
+        <div class="starter-template">
+            <h1>Bootstrap starter template</h1>
+            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        </div>
+    </div><!-- /.container -->
+
+     <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
